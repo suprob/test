dfc882e752a61c0565e93814e0c5d3331d9ed450
suprob <1002roberto@gmail.com>
2019-01-04 10:09:42 -0500

    Add files via upload

---
+++ sticky/js/main.js
@@ -0,0 +1,10 @@
+// const test = $(".one");
+
+// let sect = $(".section");
+
+// $(document).on("scroll", function() {
+//   let elementPos = sect.offset();
+//   if ($(this).scrollTop() >= elementPos.top) {
+//     test.addClass("sticky-top");
+//   }
+// });
